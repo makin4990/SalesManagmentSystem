2e3d2b3a2e8b7ea640ae2120e8adae66e6570014
Msemiz <mertsemiz1@gmail.com>
2023-09-11 19:40:06 +0300

    WebUI eklendi.

---
+++ src/WebUI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Forgot()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
